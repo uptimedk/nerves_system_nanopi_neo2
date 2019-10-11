7bd1ce6b985c583a9c364c535b7315622d60da54
Troels Brødsgaard <troels@uptime.dk>
2019-10-11 14:17:35 +0200

    Add redundant rootfs partitions and logic for upgrade and failover

--- boot.cmd
+++ boot.cmd
@@ -1,6 +1,6 @@
-setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait
+setenv bootargs console=ttyS0,115200 earlyprintk root=${uenv_root} rootwait
 
-fatload mmc 0 $kernel_addr_r Image
-fatload mmc 0 $fdt_addr_r sun50i-h5-nanopi-neo2.dtb
+fatload mmc 0:1 ${kernel_addr_r} ${bootfile}
+fatload mmc 0:1 ${fdt_addr_r} sun50i-h5-nanopi-neo2.dtb
 
-booti $kernel_addr_r - $fdt_addr_r
+booti ${kernel_addr_r} - ${fdt_addr_r}
